d65836b193ad0c9a3916485ea25b68dc8276d7f3
Rachel Harris <rachel.harris@msn.com>
2022-03-12 13:53:32 +1100

    Add engineer tests

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,3 +1,16 @@
-const Engineer = require('lib/engineer.js')
+const Engineer = require('../lib/engineer.js')
 
-// have a github profile
+describe('Engineer', () => {
+    describe('getRole', () => {
+        it('should return the engineer role', () => {
+            const result = new Engineer('Engineer', 'FooBar90');
+            expect(result.role).toBe('Engineer');
+        });
+    });
+    describe('getGithub', () => {
+        it('should return the engineer github username', () => {
+            const result = new Engineer('foo@bar.com', 'Foo Bar', '123', 'FooBar90', 'Engineer');
+            expect(result.github).toBe('FooBar90');
+        });
+    });
+});
