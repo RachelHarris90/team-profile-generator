9d62a4bd879212685fb096ea979d200d9559157a
Rachel Harris <rachel.harris@msn.com>
2022-03-12 13:57:15 +1100

    Add intern tests

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,3 +1,16 @@
-const Engineer = require('lib/intern.js')
+const Intern = require('../lib/intern.js')
 
-// have a school
+describe('Intern', () => {
+    describe('getRole', () => {
+        it('should return the interns role', () => {
+            const result = new Intern('Intern');
+            expect(result.role).toBe('Intern');
+        });
+    });
+    describe('getSchool', () => {
+        it('should return the engineer interns school', () => {
+            const result = new Intern('Intern Bar', '123', 'intern@bar.com', 'University of Sydney', 'Intern');;
+            expect(result.school).toBe('University of Sydney');
+        });
+    });
+});
